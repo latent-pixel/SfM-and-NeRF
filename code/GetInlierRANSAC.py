b759757607faed1106d8d03e35ac70e7b6bdfe6c
latent-pixel <bhargav.soothram@gmail.com>
2023-03-21 00:52:43 -0400

    Obtained inliers from the set of correspondences

---
+++ code/GetInlierRANSAC.py
@@ -0,0 +1,21 @@
+import numpy as np
+from EstimateFundamentalMatrix import estimateFundamentalMatrix
+
+
+def getInliers(matches, n_iter = 1000, eps = 0.1):
+    inliers_idx = []
+    for i in range(n_iter):
+        matches_batch = matches[np.random.choice(len(matches), 8, replace=False)]
+        F = estimateFundamentalMatrix(matches_batch)
+        s = []
+        for j in range(len(matches)):
+            x1 = np.array([matches[j][3], matches[j][4], 1.])
+            x2 = np.array([matches[j][5], matches[j][6], 1.])
+            if abs(np.dot(x2.T, np.dot(F, x1))) < eps:
+                s.append(j)
+
+        if len(inliers_idx) < len(s):
+            inliers_idx = s
+    
+    inliers_set = matches[inliers_idx]
+    return inliers_set
--- code/wrapper.py
+++ code/wrapper.py
@@ -1,4 +1,11 @@
-from getMatches import createMatchesArray
+import numpy as np
+from utils.ExtractMatchesFromText import createMatchesArray
+from EstimateFundamentalMatrix import estimateFundamentalMatrix
+from GetInlierRANSAC import getInliers
 
-matches, rgb_values = createMatchesArray('P3Data')
-print(matches.shape, rgb_values.shape)
+matches = createMatchesArray('P3Data')
+test_pts = np.array(matches[0, 1])
+print(test_pts.shape)
+# F = estimateFundamentalMatrix(test_pts)
+inliers_set = getInliers(test_pts)
+print(inliers_set.shape)
